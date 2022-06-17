453524b2742836f3d3ee577f83b7008f2cf163dd
Robin Sheen <robinnsheen@berkeley.edu>
2022-06-17 11:59:45 -0700

    created templates and pet model and addpet form

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 """Flask app for adopt app."""
 
-from flask import Flask
-
+from flask import Flask, render_template, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
-
-from models import db, connect_db
+from models import db, connect_db, Pet
+from forms import AddPetForm
 
 app = Flask(__name__)
 
@@ -12,6 +11,7 @@ app.config['SECRET_KEY'] = "secret"
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///adopt"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 connect_db(app)
 db.create_all()
@@ -22,3 +22,38 @@ db.create_all()
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 toolbar = DebugToolbarExtension(app)
+
+@app.get("/")
+def display_home():
+    """Display homepage with pets"""
+    pets = Pet.query.all()
+    return render_template("home.html", pets=pets)
+
+@app.route("/add", methods=["GET", "POST"])
+def display_add_pet_form():
+    form = AddPetForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+        available = form.available.data
+
+        pet = Pet(
+                  name=name,
+                  species=species,
+                  photo_url=photo_url,
+                  age=age,
+                  notes=notes,
+                  available=available,
+                  )
+
+        db.session.add(pet)
+        db.session.commit()
+
+        flash(f"Added {name}!")
+        return redirect("/")
+
+
--- forms.py
+++ forms.py
@@ -1,1 +1,19 @@
 """Forms for adopt app."""
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, RadioField
+
+class AddPetForm(FlaskForm):
+    """Form to add a pet"""
+
+    name = StringField("Name")
+    species = StringField("Species")
+    photo_url = StringField("Photo URL")
+    age = SelectField("Age",
+        choices=[("baby", "Baby"),
+                ("young", "Young"),
+                ("adult", "Adult"),
+                ("senior", "Senior")])
+    notes = StringField("Notes")
+    available = RadioField("Available",
+        choices=[("true", "True"), ("false", "False")],
+        coerce=bool)
